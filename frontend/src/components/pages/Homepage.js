32ff7fdf839b414d58df36aa39bdede372661403
DeeprajB <deeprajb012@gmail.com>
2023-06-13 15:04:56 +0530

    Added Sections template for Homepage

---
+++ frontend/src/components/pages/Homepage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import HeroSection from '../sections/heroSection'
+import SpecialSection from '../sections/specialSection'
+import TestimonialSection from '../sections/testimonialSection'
+import AboutSection from '../sections/aboutSection'
+
+function Homepage() {
+  return (
+    <>
+    <HeroSection />
+    <SpecialSection />
+    <TestimonialSection />
+    <AboutSection />
+    </>
+  )
+}
+
+export default Homepage
